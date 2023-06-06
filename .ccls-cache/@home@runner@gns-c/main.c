18c8e5bba9d82464a66b0146c4c144d0f30db415
fluorjo <fluorjo@naver.com>
2023-06-06 10:58:10 +0000

    c언어 배열 조심

--- .ccls-cache/@home@runner@gns-c/main.c
+++ .ccls-cache/@home@runner@gns-c/main.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 int main(void) {
 
-  int a[5]={4,3,2,1};
-  printf("%d",a[a[4]]);
+  int a[5]={5,8,11};
+  int i,s =0;
 
+  for(i=2; i<4; i++){
+    s+=a[i];
+    printf("i= %d\n",i);
+    printf("a[i]= %d\n",a[i]);
+    printf("s= %d\n",s);
+  }
+    printf("%d\n",s);
 }
